1d02e9e14d5b92d694286781724441e38a4b815d
rrrhea12138 <lirhea26@gmail.com>
2026-06-06 01:43:00 +1000

    Update ripple-mechanic.js

--- ripple-mechanic.js
+++ ripple-mechanic.js
@@ -1,1 +1,39 @@
+let img;
+let ripples = [];
 
+function preload() {
+  img = loadImage("assets/monet.jpg");
+}
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  image(img, 0, 0, width, height);
+
+  if (frameCount % 40 === 0) {
+    ripples.push({
+      x: random(width),
+      y: random(height),
+      r: 10,
+      alpha: 180
+    });
+  }
+
+  for (let i = ripples.length - 1; i >= 0; i--) {
+    let p = ripples[i];
+
+    noFill();
+    stroke(255, 255, 255, p.alpha);
+    strokeWeight(2);
+    ellipse(p.x, p.y, p.r);
+
+    p.r += 3;
+    p.alpha -= 3;
+
+    if (p.alpha <= 0) {
+      ripples.splice(i, 1);
+    }
+  }
+}
